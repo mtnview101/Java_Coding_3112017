4cd2684277046a112bbe1a4692953ec19ae84190
mtnview101 <mtnview101@gmail.com>
2017-03-11 16:38:57 -0800

    commit 2

---
+++ src/main/java/core/BenchmarkingStrings.java
@@ -0,0 +1,30 @@
+package core;
+
+public class BenchmarkingStrings {
+	public static void main(String[] args) {
+        String string = "a";
+        StringBuffer sbffr = new StringBuffer("a");
+        StringBuilder sbldr = new StringBuilder("a");
+        int max = 100000;
+        long start = System.currentTimeMillis();
+        	for (int i = 0; i < max; i++) string = string.concat("b");
+        long finish = System.currentTimeMillis();
+        long result = (finish - start);
+  System.out.println("Using String, elapsed milliseconds: " + result);
+  
+start = System.currentTimeMillis();
+for (int i = 0; i < max; i++) sbffr = sbffr.append("b");
+finish = System.currentTimeMillis();
+result = (finish - start);
+System.out.println("Using StringBuffer, elapsed milliseconds: " + result);
+
+start = System.currentTimeMillis();
+for (int i = 0; i < max; i++) sbldr = sbldr.append("b");
+finish = System.currentTimeMillis();
+result = (finish - start);
+System.out.println("Using StringBuilder, elapsed milliseconds: " + result);
+  
+  
+}
+}
+
